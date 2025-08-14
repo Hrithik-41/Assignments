17721d81ff85de1f8097e41c3aeecb7e62f480b8
Hrithik-41 <littlehrithik9594@gmail.com>
2025-08-14 13:58:52 +0530

    Update Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,19 @@
+#Assignment 1
+# first_number = int(input("Enter first number : "))
+# second_number = int(input("Enter second number : "))
+#
+# addition = first_number + second_number
+# subtraction = first_number - second_number
+# multiplication = first_number * second_number
+# division = first_number / second_number
+#
+# print("Addition : ", addition)
+# print("Subtraction : ", subtraction)
+# print("Multiplication : ", multiplication)
+# print("Division : ", division)
+
+#Assignment 2
+# first_name = input("Enter first name : ")
+# last_name = input("Enter last name : ")
+# full_name = first_name + " " + last_name
+# print("Hello,",full_name+"!", "Welcome to the Python program.")
