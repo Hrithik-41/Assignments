884996d2b30664b196f8da47fb79ce7edbeb8a47
Hrithik-41 <82141694+Hrithik-41@users.noreply.github.com>
2025-08-28 01:59:56 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -40,17 +40,47 @@
 
 #Assignment 3
 #Task 1
-'''num = int(input("Enter a number : "))
-def factorial(num):
-    if num < 2:
-        return 1
-    else:
-        return num * factorial(num - 1)
-print("Factorial of", num, "is", factorial(num))'''
+# num = int(input("Enter a number : "))
+# def factorial(num):
+#     if num < 2:
+#         return 1
+#     else:
+#         return num * factorial(num - 1)
+# print("Factorial of", num, "is", factorial(num))
 
 #Task 2
-'''import math
-num = float(input("Enter Your Number : "))
-print("Square Root :", math.sqrt(num))
-print("Logarithm :", math.log(num))
-print("Sine :", math.sin(num))'''
+# import math
+# num = float(input("Enter Your Number : "))
+# print("Square Root :", math.sqrt(num))
+# print("Logarithm :", math.log(num))
+# print("Sine :", math.sin(num))
+
+
+
+#Assignment 4
+'''Task 1
+try :
+    file = open("sample.txt", "r")
+    read_file = file.read()
+    print(read_file)
+except FileNotFoundError:
+    print("The file 'sample.txt' was not found")'''
+
+#Task 2
+'''text = input('Enter text to write to the file : ')
+file = open('test', 'w')
+print("Data successfully written to output.txt")
+file.write(text)
+file.close()
+
+text = input('Enter additional text to append : ')
+file= open('test', 'a')
+write_file2 = file.write(text)
+print("Data successfully appended")
+file.close()
+
+file = open('test', 'r')
+text = file.read()
+print("Final content of output.txt")
+print(text)
+file.close()'''
